3e0264c4a3f33e8f2ddabe32fdbc3d3f43b92721
ethang404 <ethangordon25@gmail.com>
2026-01-19 17:07:59 -0600

    Added centralized error throws to workout service file. Fixed EditWorkout endpoint

--- backend/Workouts/service.js
+++ backend/Workouts/service.js
@@ -4,8 +4,13 @@ const SetsModel = require("../models/modelInits").sets;
 const UsersModel = require("../models/modelInits").users;
 
 //error imports
-const {GeneralError, NotFoundError, DataError, UnauthorizedError, ForbiddenError} = require("../error")
-
+const {
+	GeneralError,
+	NotFoundError,
+	DataError,
+	UnauthorizedError,
+	ForbiddenError,
+} = require("../error");
 
 async function GetWorkouts(user_id, filter) {
 	let workouts;
@@ -37,12 +42,11 @@ async function GetWorkouts(user_id, filter) {
 async function CreateWorkout(data, user_id) {
 	//createWorkout
 	const user = await UsersModel.findOne({ where: { user_id: user_id } });
-	if (!user)
-		throw new GeneralError("No user found or authentication expired")//this shouldn't even hit
+	if (!user) throw new GeneralError("No user found or authentication expired"); //this shouldn't even hit
 
 	if (!data.workout_name || !data.workout_date)
-		throw new DataError("Workout Name and Workout Date required")
-		
+		throw new DataError("Workout Name and Workout Date required");
+
 	var workout = await user.createWorkout({
 		name: data.workout_name,
 		workout_date: data.workout_date,
@@ -56,24 +60,27 @@ async function CreateWorkout(data, user_id) {
 		newExercise = await workout.createExercise({
 			exercise_name: exercise.exercise_name,
 			notes: exercise.notes,
-			order_number: exercise.order_number
+			order_number: exercise.order_number,
 		});
 
 		if (newExercise)
-			console.log(`Created new exercise with id ${newExercise.exercise_id} (${exercise.exercise_name})`)
+			console.log(
+				`Created new exercise with id ${newExercise.exercise_id} (${exercise.exercise_name})`,
+			);
 
 		for (let set of exercise.sets) {
 			let newSet;
 			newSet = newExercise.createSet({
-				set_number: set.set_number,//note set_number is NOT primary key, just numbering of sets (0 is warmup, 1+ is working)
+				set_number: set.set_number, //note set_number is NOT primary key, just numbering of sets (0 is warmup, 1+ is working)
 				notes: set.notes,
 				reps: set.reps,
 				weight: set.weight,
-				order_number: exercise.order_number
+				set_type: set.set_type, //"warmup" vs "working" etc.
+				order_number: set.order_number, //order of sets (1-x)
 			});
-			
+
 			if (newExercise)
-				console.log(`Created new set with id ${newSet.set_id} (for ${exercise.exercise_id})`)
+				console.log(`Created new set with id ${newSet.set_id} (for ${exercise.exercise_id})`);
 		}
 	}
 
@@ -82,7 +89,7 @@ async function CreateWorkout(data, user_id) {
 
 async function EditWorkout(data, workout_id) {
 	//console.log("workout id: ", workout_id)
-	let workout_obj = await WorkoutsModel.findByPk(workout_id)
+	let workout_obj = await WorkoutsModel.findByPk(workout_id);
 	//console.log("Did I find my workout?", workout_obj)
 
 	/* let workout = await workout_obj.update({
@@ -99,168 +106,114 @@ async function EditWorkout(data, workout_id) {
 
 	//if we have exercise/set_id in database that isn't in workout, remove it from database
 
-	if (!workout_obj)
-		throw new NotFoundError("No workout found with that id")
+	if (!workout_obj) throw new NotFoundError("No workout found with that id");
 
-	const exercisesList = await ExercisesModel.findAll({where: {workout_id:workout_id}, include: SetsModel});
+	const exercisesList = await ExercisesModel.findAll({
+		where: { workout_id: workout_id },
+		include: SetsModel,
+	});
 	//cast exercises and sets into a map
-	let javascriptArray = exercisesList.map(exercise => exercise.toJSON());
+	let javascriptArray = exercisesList.map((exercise) => exercise.toJSON());
 
 	//console.log("JSON converted: ", javascriptArray)
 
 	//If object in database but not in passed data: remove
-	for (let dbExercise of javascriptArray){
+	for (let dbExercise of javascriptArray) {
 		//if exercise id in database not found in data: remove
-		let shouldExist = data.exercise.some(exercise => exercise.exercise_id === dbExercise.exercise_id)
-		if (!shouldExist){
-			console.log(`Removing exercise: ${dbExercise.exercise_id} from database`)
-			await ExercisesModel.destroy({where: {exercise_id:dbExercise.exercise_id}})
-		}
-		
-		//now check each set id of each exercise to see if it should exist
-		for (let dbSet of dbExercise.sets){
-			shouldExist = data.exercise.some(set => set.set_id === dbSet.set_id)
-			if (!shouldExist){
-				console.log(`Removing set: ${dbSet.set_id} from database`)
-				await SetsModel.destroy({where: {set_id:dbSet.set_id}})
+		let shouldExist = data.exercises.some(
+			(exercise) => exercise.exercise_id === dbExercise.exercise_id,
+		);
+		if (!shouldExist) {
+			console.log(`Removing exercise: ${dbExercise.exercise_id} from database`);
+			await ExercisesModel.destroy({ where: { exercise_id: dbExercise.exercise_id } });
+		} else {
+			//now check each set id of each exercise we DIDN'T REMOVE to see if it should exist
+			let exerciseSets = data.exercises.find(
+				(exercise) => exercise.exercise_id === dbExercise.exercise_id,
+			);
+			for (let dbSet of dbExercise.sets) {
+				shouldExist = exerciseSets.sets.some((set) => set.set_id === dbSet.set_id);
+				if (!shouldExist) {
+					console.log(`Removing set: ${dbSet.set_id} from database`);
+					await SetsModel.destroy({
+						where: { set_id: dbSet.set_id, exercise_id: dbExercise.exercise_id },
+					});
+				}
 			}
 		}
 	}
-	
-	//stop here and make sure my things are actually being deleted before I try to add new ones
-	return null
-	console.log("Workout data before update:")
-	console.log("------------------------------")
-	console.log(JSON.stringify(exercisesList, null, 2));
-	console.log("------------------------------")
 
+	//stop here and make sure my things are actually being deleted before I try to add new ones
+	//return null;
+	/*console.log("Workout data before update:");
+	console.log("------------------------------");
+	console.log(JSON.stringify(exercisesList, null, 2));
+	console.log("------------------------------");*/
+	console.log("EVERYTHING WORKS SO FAR");
 	for (let exercise of data.exercises) {
-		let exercise_obj = await ExercisesModel.findByPk(exercise.exercise_id)
-		if (exercise_obj == null){
+		let exercise_obj = await ExercisesModel.findByPk(exercise.exercise_id);
+		if (exercise_obj == null) {
 			//we have a new exercise
-			let newExercise = await workout.createExercise({
+			let newExercise = await workout_obj.createExercise({
 				exercise_name: exercise.exercise_name,
 				notes: exercise.notes,
+				order_number: exercise.order_number,
 			});
 
 			//create corresponding new sets for newly added exercise
 			for (let set of exercise.sets) {
 				await newExercise.createSet({
-					set_number: set.set_number,//note set_number is NOT primary key, just numbering of sets (0 is warmup, 1+ is working)
+					set_type: set.set_type,
+					order_number: set.order_number,
+					set_number: set.set_number, //note set_number is NOT primary key, just numbering of sets (0 is warmup, 1+ is working)
 					notes: set.notes,
 					reps: set.reps,
 					weight: set.weight,
 				});
 			}
-		}
-		
-		else{
+		} else {
 			//existing exercise to update
-			let updatedExercise = exercise_obj.update({
+			let updatedExercise = await exercise_obj.update({
 				exercise_name: exercise.exercise_name,
+				order_number: exercise.order_number,
 				notes: exercise.notes,
 			});
 
 			//loop over data exercise's sets to update/add
 			for (let set of exercise.sets) {
-				let set_obj = await ExercisesModel.findByPk(set.set_id)
-				if (set_obj == null){
+				let set_obj = await SetsModel.findByPk(set.set_id);
+				if (set_obj == null) {
 					//we have a new set for exercise
 					await exercise_obj.createSet({
-						set_number: set.set_number,//note set_number is NOT primary key, just numbering of sets (0 is warmup, 1+ is working)
+						set_type: set.set_type,
+						order_number: set.order_number,
+						set_number: set.set_number, //note set_number is NOT primary key, just numbering of sets (0 is warmup, 1+ is working)
 						notes: set.notes,
 						reps: set.reps,
 						weight: set.weight,
 					});
-				}
-				else{
+				} else {
 					await set_obj.update({
+						set_type: set.set_type,
+						order_number: set.order_number,
 						set_number: set.set_number,
 						notes: set.notes,
 						reps: set.reps,
-						weight: set.weight
-					})
-				}
-			}
-		}
-	}
-
-	const exercisesListAfter = await ExercisesModel.findAll({where: {workout_id:workout_id}, include: SetsModel});
-	console.log("Workout data AFTER update:")
-	console.log("------------------------------")
-	console.log(JSON.stringify(exercisesListAfter, null, 2));
-	console.log("------------------------------")
-
-}
-
-//delete later..keeping  for reference just in case:
-async function EditWorkout2(data, user_id, workout_id) {
-
-	let workout_obj = await WorkoutsModel.findByPk(workout_id)
-
-	let workout = await workout_obj.update({
-		name: data.workout_name,
-		workout_date: data.workout_date,
-		notes: data.notes,
-		finished_at: data.finished_at,
-	});
-	
-
-	//update exercises per workout and sets per exercise
-	for (let exercise of data.exercises) {
-		let exercise_obj = await ExercisesModel.findByPk(exercise.exercise_id)
-		if (exercise_obj == null){
-			//new exercise
-			let newExercise = await workout.createExercise({
-				exercise_name: exercise.exercise_name,
-				notes: exercise.notes,
-			});
-
-			//add corresponding sets for that exercise
-			for (let set of exercise.sets) {
-				newExercise.createSet({
-					set_number: set.set_number,//note set_number is NOT primary key, just numbering of sets (0 is warmup, 1+ is working)
-					notes: set.notes,
-					reps: set.reps,
-					weight: set.weight,
-				});
-			}
-
-
-		}
-		else{
-			let updatedExercise = exercise_obj.update({
-				exercise_name: exercise.exercise_name,
-				notes: exercise.notes,
-			});
-		}
-
-		for (let set of exercise.sets) {
-			let set_obj = await SetsModel.findByPk(set.set_id)
-			if (set_obj === null){
-				//new set was added
-				//add corresponding sets for that exercise
-				for (let set of exercise.sets) {
-					newExercise.createSet({
-						set_number: set.set_number,//note set_number is NOT primary key, just numbering of sets (0 is warmup, 1+ is working)
-						notes: set.notes,
-						reps: set.reps,
 						weight: set.weight,
 					});
 				}
-
 			}
-			set_obj.update({
-				set_number: set.set_number,
-				notes: set.notes,
-				reps: set.reps,
-				weight: set.weight,
-			});
 		}
 	}
 
-	return workout;
-
+	const exercisesListAfter = await ExercisesModel.findAll({
+		where: { workout_id: workout_id },
+		include: SetsModel,
+	});
+	console.log("Workout data AFTER update:");
+	console.log("------------------------------");
+	console.log(JSON.stringify(exercisesListAfter, null, 2));
+	console.log("------------------------------");
 }
 
-module.exports = { GetWorkouts, CreateWorkout, EditWorkout};
+module.exports = { GetWorkouts, CreateWorkout, EditWorkout };
